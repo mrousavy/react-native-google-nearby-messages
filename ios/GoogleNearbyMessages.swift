cb6c49478765a0826db7b3319d1f10ca23ae6dfb
Marc Rousavy <marcrousavy@hotmail.com>
2020-06-02 21:24:39 +0200

    implement ios

--- ios/GoogleNearbyMessages.swift
+++ ios/GoogleNearbyMessages.swift
@@ -10,8 +10,21 @@ import Foundation
 import GNSMessages
 
 @objc(NearbyMessages)
-class NearbyMessages: NSObject {
-	private var count = 0
+class NearbyMessages: RCTEventEmitter {
+	enum EventType: String {
+		case MESSAGE_FOUND
+		case MESSAGE_LOST
+		case BLUETOOTH_ERROR
+		case PERMISSION_ERROR
+	}
+	enum GoogleNearbyMessagesError: Error {
+		case runtimeError(String)
+	}
+
+	
+	private var messageManager: GNSMessageManager = nil
+	private var currentPublication = nil
+	private var currentSubscription = nil
 	
 	@objc(constantsToExport)
 	public func constantsToExport() -> [AnyHashable : Any]! {
@@ -19,12 +32,89 @@ class NearbyMessages: NSObject {
 	}
 	
 	@objc
-	func connect(apiKey: String, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> GNSMessageManager {
-		let messageManager = GNSMessageManager(APIKey: apiKey)
-		return messageManager
+	func connect(apiKey: String, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
+		do {
+			// TODO: remove debug logging
+			GNSMessageManager.setDebugLoggingEnabled(true)
+			
+			let hasPermission = GNSPermission.isGranted()
+			if (!hasPermission) {
+				throw GoogleNearbyMessagesError.runtimeError("BLE Permission denied!")
+			}
+			
+			self.messageManager = GNSMessageManager(APIKey: apiKey,
+													paramsBlock: { (params: GNSMessageManagerParams?) in
+														guard let params = params else { return }
+														params.microphonePermissionErrorHandler = { (hasError: Bool) in
+															sendEvent(withName: EventType.PERMISSION_ERROR.rawValue, body: [ "permission": "microphone" ]);
+														}
+														params.bluetoothPowerErrorHandler = { (hasError: Bool) in
+															sendEvent(withName: EventType.BLE_ERROR.rawValue, body: [ "hasError": hasError ]);
+														}
+														params.bluetoothPermissionErrorHandler = { (hasError: Bool) in
+															sendEvent(withName: EventType.PERMISSION_ERROR.rawValue, body: [ "message": "bluetooth" ]);
+														}
+														params.strategy = GNSStrategy(paramsBlock: { (params: GNSStrategyParams?) in
+															guard let params = params else { return }
+															// TODO: currently it only supports BLE. Maybe support more?
+															params.discoveryMediums = .BLE
+														})
+			})
+			resolve(nil)
+		} catch {
+			reject(error)
+		}
 	}
 	
-	// TODO: https://developers.google.com/nearby/messages/ios/pub-sub#swift
+	@objc
+	func disconnect(resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
+		self.currentSubscription = nil
+		self.currentPublication = nil
+		self.messageManager = nil
+		resolve(nil)
+	}
+	
+	@objc
+	func publish(message: String, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
+		do {
+			self.currentPublication = try self.messageManager.publication(with: GNSMessage(content: message.data(using: .utf8)))
+			resolve(nil)
+		} catch {
+			reject(error)
+		}
+	}
+	
+	@objc
+	func unpublish(resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
+		self.currentPublication = nil
+		resolve(nil)
+	}
+	
+	@objc
+	func subscribe(resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
+		do {
+			self.currentSubscription = messageManager.subscription(
+				messageFoundHandler: { (message: GNSMessage?) in
+					sendEvent(withName: EventType.MESSAGE_FOUND.rawValue, body: [ "message": message ]);
+				},
+				messageLostHandler: { (message: GNSMessage?) in
+				sendEvent(withName: EventType.MESSAGE_LOST.rawValue, body: [ "message": message ]);
+				})
+			resolve(nil)
+		} catch {
+			reject(error)
+		}
+	}
+	
+	@objc
+	func unsubscribe(resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
+		self.currentSubscription = nil
+		resolve(nil)
+	}
+	
+	override func supportedEvents() -> [String]! {
+		return ["MESSAGE_FOUND", "MESSAGE_LOST", "BLUETOOTH_ERROR", "PERMISSION_ERROR"]
+	}
 	
 	@objc
 	static func requiresMainQueueSetup() -> Bool {
--- ios/GoogleNearbyMessagesBridge.m
+++ ios/GoogleNearbyMessagesBridge.m
@@ -7,10 +7,15 @@
 //
 
 #import "GoogleNearbyMessagesBridge.h"
-#import <React/RCTBridgeModule.h>
+#import <RCTBridgeModule.h>
 
 @interface RCT_EXTERN_REMAP_MODULE(GoogleNearbyMessages, NearbyMessages, NSObject)
 
-RCT_EXTERN_METHOD(increment);
+RCT_EXTERN_METHOD(connect);
+RCT_EXTERN_METHOD(disconnect);
+RCT_EXTERN_METHOD(publish);
+RCT_EXTERN_METHOD(unpublish);
+RCT_EXTERN_METHOD(subscribe);
+RCT_EXTERN_METHOD(unsubscribe);
 
 @end
